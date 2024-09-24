d7854020fba8eb2eb339e1d475a207cfaa2f5fc8
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-09-24 21:13:23 +0200

    update

--- main.js
+++ main.js
@@ -1,116 +1,76 @@
-// const btnsWrapper = document.querySelector('.btns-numbers-wrapper')
-// const mainCalculationInput = document.querySelector('.main-calculation')
-
-// const btns = ['0','1','2','3','4','5','6','7','8','9','+','-','/','*','=', '.','C']
-
-// let leftCalc;
-// let rightCalc;
-// let operator;
-// let calculation;
-// let decimal;
-
-// function createBtns(){
-//   btns.forEach((btn)=>{
-//     const btn_el = document.createElement('button')
-//     btn_el.classList.add('numberBtns')
-//     btn_el.textContent = btn
-//     btnsWrapper.append(btn_el)
-//   })
-//   triggerCalculation()
-// }
-
-// function triggerCalculation(){
-//   btnsWrapper.addEventListener('click', (e)=>{
-//     const getTextTarget = e.target.textContent
-//     mainCalculationInput.value += getTextTarget;
-//     const getMainInputValue = mainCalculationInput.value;
-//     waitForFullCalculation(getTextTarget, getMainInputValue)
-//   })
-// }
-
-// function waitForFullCalculation(getTextTarget, getMainInputValue){
-//   const smallCalculationInput = document.querySelector('.small-calculation')
-//   if(getTextTarget==='+'){
-//     leftCalc = parseInt(getMainInputValue.slice(0, -1))
-//     operator = '+'
-//     smallCalculationInput.value = getMainInputValue
-//     mainCalculationInput.value = ''  
-//   }else if(getTextTarget==='-'){
-//     leftCalc = parseInt(getMainInputValue.slice(0, -1))
-//     operator = '-'
-//     smallCalculationInput.value = getMainInputValue
-//     mainCalculationInput.value = ''
-//   }else if(getTextTarget==='*'){
-//     leftCalc = parseInt(getMainInputValue.slice(0, -1))
-//     operator = '*'
-//     smallCalculationInput.value = getMainInputValue
-//     mainCalculationInput.value = ''
-//   }else if(getTextTarget==='/'){
-//     leftCalc = parseInt(getMainInputValue.slice(0, -1))
-//     operator = '/'
-//     smallCalculationInput.value = getMainInputValue
-//     mainCalculationInput.value = ''
-//   }else if(getTextTarget==='C'){
-//    operator = ''
-//    leftCalc = ''
-//    rightCalc = ''
-//    smallCalculationInput.value = ''
-//    mainCalculationInput.value = ''
-//   }
-//   else if(getTextTarget==='='){
-//     rightCalc = parseInt(getMainInputValue.slice(0, -1))
-//     smallCalculationInput.value += rightCalc;
-//     if(operator==='+'){
-//       calculation = leftCalc + rightCalc;
-//       smallCalculationInput.value = `${leftCalc} ${operator} ${rightCalc} = ${calculation}`
-//       mainCalculationInput.value = calculation
-//     }else if(operator==='-'){
-//       calculation = leftCalc - rightCalc;
-//       smallCalculationInput.value = `${leftCalc} ${operator} ${rightCalc} = ${calculation}`
-//       mainCalculationInput.value = calculation
-//     }else if(operator==='*'){
-//       calculation = leftCalc * rightCalc;
-//       smallCalculationInput.value = `${leftCalc} ${operator} ${rightCalc} = ${calculation}`
-//       mainCalculationInput.value = calculation
-//     }else if(operator==='/'){
-//       calculation = leftCalc / rightCalc;
-//       smallCalculationInput.value = `${leftCalc} ${operator} ${rightCalc} = ${calculation}`
-//       mainCalculationInput.value = calculation
-//     }
-//   }
-// }
-
-// createBtns()
-
-
-
-
-
-
-
-
-
-
-
-
-
 const btns_numbers_wrapper = document.querySelector('.btns-numbers-wrapper')
 const small_calculation = document.querySelector('.small-calculation')
 const main_calculation = document.querySelector('.main-calculation')
 
 const btns = ['0','1','2','3','4','5','6','7','8','9','+','-','/','*','C','.','=']
 
-let leftCalc;
-let rightCalc;
+let calculation = []
 let operator;
-
-let firstCalc = []
 
 function calculate(e){
   const getTextTarget = e.target.textContent;
   main_calculation.value += getTextTarget;
 
-  
+  if(getTextTarget==='+'){
+    operator = '+'
+    small_calculation.value = main_calculation.value;
+    main_calculation.value = ''
+    calculation.push(parseFloat(small_calculation.value))
+    const totalAdd = calculation.reduce((accum, curVal)=>accum+curVal)
+    small_calculation.value = totalAdd + '+'
+  }else if(getTextTarget==='='){
+    if(operator==='+'){
+      const rightSide = parseFloat(main_calculation.value)
+      const leftSide = parseFloat(small_calculation.value)
+      const total = leftSide + rightSide;
+      small_calculation.value = leftSide + '+' + rightSide + '=' + total;
+      main_calculation.value = total
+    }else if(operator==='-'){
+      const rightSide = parseFloat(main_calculation.value)
+      const leftSide = parseFloat(small_calculation.value)
+      const total = leftSide - rightSide;
+      small_calculation.value = leftSide + '-' + rightSide + '=' + total;
+      main_calculation.value = total
+    }else if(operator==='*'){
+      const leftSide = parseFloat(small_calculation.value)
+      const rightSide = parseFloat(main_calculation.value)
+      const total = leftSide * rightSide;
+      small_calculation.value = leftSide + '*' + rightSide + '=' + total;
+      main_calculation.value = total
+    }else if(operator==='/'){
+      const leftSide = parseFloat(small_calculation.value)
+      const rightSide = parseFloat(main_calculation.value)
+      const total = leftSide / rightSide;
+      small_calculation.value = leftSide + '/' + rightSide + '=' + total;
+      main_calculation.value = total
+    }
+  }else if(getTextTarget==='-'){
+    operator = '-'
+    small_calculation.value = main_calculation.value;
+    main_calculation.value = ''
+    calculation.push(parseFloat(small_calculation.value))
+    const total = calculation.reduce((accum, curVal)=>accum-curVal)
+    small_calculation.value = total + '-'
+  }else if(getTextTarget==='*'){
+    operator = '*'
+    small_calculation.value = main_calculation.value;
+    main_calculation.value = ''
+    calculation.push(parseFloat(small_calculation.value))
+    const total = calculation.reduce((accum, curVal)=>accum*curVal)
+    small_calculation.value = total + '*'
+  }else if(getTextTarget==='/'){
+    operator = '/'
+    small_calculation.value = main_calculation.value
+    main_calculation.value = ''
+    calculation.push(parseInt(small_calculation.value))
+    const total = calculation.reduce((accum, curVal)=>accum/curVal)
+    small_calculation.value = total + '/'
+  }else if(getTextTarget==='C'){
+    small_calculation.value = ''
+    main_calculation.value = ''
+    calculation = []
+    operator = ''
+  }
 }
 
 btns.forEach((btn)=>{
